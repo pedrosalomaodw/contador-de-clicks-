d9f13675a7927b180b1263c81a7fd002c962c1c3
Pedro Josué <74951282+pedrojosuesalomao@users.noreply.github.com>
2023-10-28 23:00:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+var numero = 0
+function conta(){
+       numero++
+       document.getElementById('cont').innerHTML = numero
+}
+
+function del(){
+    numero = 0 
+    document.getElementById('cont').innerHTML = numero
+}
+
+//@pedrosalomao_
